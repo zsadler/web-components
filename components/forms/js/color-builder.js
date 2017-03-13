4319d67651085d70281bf0995522e2b8c383fbcc
Zoe Sadler <zsadler@gmail.com>
2017-03-12 20:39:12 -0400

    color mixer js

--- components/forms/js/color-builder.js
+++ components/forms/js/color-builder.js
@@ -1,10 +1,48 @@
 (function (window) {
-    var colorBuilder = document.querySelector('#colorBuilder');
+    // Set all the on page laod defaults
+    var colorBuilder = document.querySelector('#colorBuilder'),
+        rgbaGeneratedEl = document.querySelector('#color-generated'),
+        rRangeEl = document.querySelector('#rbg-r'),
+        gRangeEl = document.querySelector('#rbg-g'),
+        bRangeEl = document.querySelector('#rbg-b'),
+        aRangeEl = document.querySelector('#rgb-alpha'),
+        rValueEl = document.querySelector('.rbg-r span'),
+        gValueEl = document.querySelector('.rbg-g span'),
+        bValueEl = document.querySelector('.rbg-b span'),
+        aValueEl = document.querySelector('.rgb-alpha span'),
+        rRange = rRangeEl.value,
+        gRange = gRangeEl.value,
+        bRange = bRangeEl.value,
+        aRange = aRangeEl.value,
+        rgbaGenerated = 'background-Color:rgba('+ rRange +','+ gRange +','+ bRange +','+ aRange +')';
+
+    // set generated colors default values
+    rgbaGeneratedEl.setAttribute("style", rgbaGenerated);
+    
+    // set labels default numeric values
+    rValueEl.innerHTML = rRange;
+    gValueEl.innerHTML = gRange;
+    bValueEl.innerHTML = bRange;
+    aValueEl.innerHTML = aRange;
 
     // colorBuilder form
     if(colorBuilder !== null) {
-        colorBuilder.addEventListener('submit', function(e){    
+        colorBuilder.addEventListener('change', function(e){
+            // get range values
+            rRange = rRangeEl.value;
+            gRange = gRangeEl.value;
+            bRange = bRangeEl.value;
+            aRange = aRangeEl.value;
 
+            // set labels numeric value
+            rValueEl.innerHTML = rRange;
+            gValueEl.innerHTML = gRange;
+            bValueEl.innerHTML = bRange;
+            aValueEl.innerHTML = aRange;    
+
+            // set value on range change
+            rgbaGenerated = 'background-Color:rgba('+ rRange +','+ gRange +','+ bRange +','+ aRange +')';
+            rgbaGeneratedEl.setAttribute("style", rgbaGenerated);
         });
     }
 })(window);
